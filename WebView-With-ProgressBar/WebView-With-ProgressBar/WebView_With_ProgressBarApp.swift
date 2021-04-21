1fd423c9afe93c0f29ab7a3a6365e0f4c109e5c2
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2021-04-21 21:38:13 +0900

    初回コミット

---
+++ WebView-With-ProgressBar/WebView-With-ProgressBar/WebView_With_ProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_With_ProgressBarApp.swift
+//  WebView-With-ProgressBar
+//
+//  Created by home on 2021/04/21.
+//
+
+import SwiftUI
+
+@main
+struct WebView_With_ProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
